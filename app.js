e553ffa4eb3db93efb13e071cfda78b1b9063c4f
Madhurya Bharadwaaz <dg.hipl@gmail.com>
2023-04-19 23:38:18 +0530

    testing nodejs server deployment to netlify

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { questions, names, people } = require('./constants');
+const app = express()
+
+app.get('/questions', (req, res) => {
+    res.send(questions);
+})
+
+app.get('/names', (req, res) => {
+    res.send(names);
+})
+
+app.get('/people', (req, res) => {
+    res.send(people);
+})
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000')
+})
---
+++ constants.js
@@ -0,0 +1,173 @@
+export const names = ["Derek", "Joe", "Anna", "Coen", "Chris", "Macey", "Ella"];
+
+export const questions = [
+    {
+        "id": "sign-up-form",
+        "name": "Sign-Up Form",
+        "category": "HTML"
+    },
+    {
+        "id": "item-cart",
+        "name": "Item Cart",
+        "category": "HTML"
+    },
+    {
+        "id": "spaghetti-recipe",
+        "name": "Spaghetti Recipe",
+        "category": "HTML"
+    },
+    {
+        "id": "blog-post",
+        "name": "Blog Post",
+        "category": "HTML"
+    },
+    {
+        "id": "rainbow-circles",
+        "name": "Rainbow Circles",
+        "category": "CSS"
+    },
+    {
+        "id": "navbar",
+        "name": "Navbar",
+        "category": "CSS"
+    },
+    {
+        "id": "pig-emoji",
+        "name": "Pig Emoji",
+        "category": "CSS"
+    },
+    {
+        "id": "purchase-form",
+        "name": "Purchase Form",
+        "category": "CSS"
+    },
+    {
+        "id": "algoexpert-products",
+        "name": "AlgoExpert Products",
+        "category": "CSS"
+    },
+    {
+        "id": "testing-framework",
+        "name": "Testing Framework",
+        "category": "JavaScript"
+    },
+    {
+        "id": "array-methods",
+        "name": "Array Methods",
+        "category": "JavaScript"
+    },
+    {
+        "id": "event-target",
+        "name": "Event Target",
+        "category": "JavaScript"
+    },
+    {
+        "id": "debounce",
+        "name": "Debounce",
+        "category": "JavaScript"
+    },
+    {
+        "id": "this-binding",
+        "name": "This Binding",
+        "category": "JavaScript"
+    },
+    {
+        "id": "promisify",
+        "name": "Promisify",
+        "category": "JavaScript"
+    },
+    {
+        "id": "throttle",
+        "name": "Throttle",
+        "category": "JavaScript"
+    },
+    {
+        "id": "curry",
+        "name": "Curry",
+        "category": "JavaScript"
+    },
+    {
+        "id": "infinite-scroll",
+        "name": "Infinite Scroll",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "stopwatch",
+        "name": "Stopwatch",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "tic-tac-toe",
+        "name": "Tic Tac Toe",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "todo-list",
+        "name": "Todo List",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "typeahead",
+        "name": "Typeahead",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "tier-list",
+        "name": "Tier List",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "toasts",
+        "name": "Toasts",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "sudoku",
+        "name": "Sudoku",
+        "category": "DOM Manipulation"
+    },
+    {
+        "id": "questions-list",
+        "name": "Questions List",
+        "category": "DOM Manipulation"
+    }
+]
+
+export const people = [
+    {
+        id: 1,
+        name: "Chris",
+        age: 24,
+        favoriteNumbers: [1, 4, 8, 12, 32],
+    },
+    {
+        id: 2,
+        name: "Terrance",
+        age: 37,
+        favoriteNumbers: [2, 8, 9],
+    },
+    {
+        id: 3,
+        name: "Megan",
+        age: 22,
+        favoriteNumbers: [23, 14],
+    },
+    {
+        id: 4,
+        name: "Juan",
+        age: 18,
+        favoriteNumbers: [23, 14, 2],
+    },
+    {
+        id: 5,
+        name: "Tina",
+        age: 42,
+        favoriteNumbers: [12, 9, 1, 4, 18],
+    },
+    {
+        id: 6,
+        name: "Lin",
+        age: 24,
+        favoriteNumbers: [3, 9],
+    },
+];
